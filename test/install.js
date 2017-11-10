9aef89a11b073a15951829c4a2c3bc81b8110d51
Volker Buzek <volker.buzek@js-soft.com>
2017-11-10 09:21:55 +0100

    fix(windows,zip-error): download doesn't corrupt .zip, fixes #4
    - download() now uses npm module request instead of plain http/s
    - accompanying unit test
    - test setup: proxyquire not caching stubs any more
    - helpers.download: rethrowing rejected Promise to correctly bubble
      up in the surrounding Promise chain

--- test/install.js
+++ test/install.js
@@ -1,6 +1,6 @@
 const path = require('path');
 const expect = require('chai').expect;
-const proxyquire = require('proxyquire');
+const proxyquire = require('proxyquire').noPreserveCache();
 const fs = require('fs-extra');
 
 describe("from parameter", function () {
@@ -58,78 +58,89 @@ describe("to parameter", function () {
         let to = path.join(root, "ui5");
         expect(helpers.unzip.bind(this, to)).to.throw();
     });
-    it("should throw an error if to path is not inside $project_dir", function() {
+    it("should throw an error if to path is not inside $project_dir", function () {
         const helpers = require('../lib/helpers');
         let to = path.join(process.cwd(), "..", "..", "some", "path");
         expect(helpers.unzip.bind(this, to)).to.throw();
     })
 });
 
-describe("install", function() {
+describe("install", function () {
     const glob = require('glob');
     const constants = require('../lib/constants.js');
     const {install} = proxyquire('../lib/commands', {
         "./helpers": {
             download(from) {
-                return Promise.resolve().then( () => {
+                return Promise.resolve().then(() => {
                     let mockZip = path.join(process.cwd(), "test", "example.zip");
                     fs.copySync(mockZip, constants.tmpDownloadFile);
                 })
             },
-            addEntryToXmlFileAt(a,b,c) {
+            addEntryToXmlFileAt(a, b, c) {
                 return true;
             }
         },
         "fs-extra": {
-            emptyDir: function(target) {
+            emptyDir: function (target) {
                 return true;
             },
-            ensureDirSync: function(target) {
+            ensureDirSync: function (target) {
                 return true;
             },
-            writeFileSync: function(src, target) {
+            writeFileSync: function (src, target) {
                 return true;
             },
-            existsSync: function(file) {
+            existsSync: function (file) {
                 return true;
             }
         }
     });
-    
+
     it("should download a mocked UI5 lib from an URL to a custom location in the $project_dir", function (done) {
         let fromMock = "http://mocked/zip.zip";
         let to = "local/ui5/dir";
         let source = path.join(process.cwd(), to);
-        Promise.resolve().then( () => {
+        Promise.resolve().then(() => {
             return install(fromMock, to);
-        }).then( () => {
+        }).then(() => {
             let iFiles = glob.sync(`${source}/**/*dbg.js`).length;
             expect(iFiles).to.be.greaterThan(0);
             fs.removeSync(source);
             done();
-        }).catch( err => {
+        }).catch(err => {
             fs.removeSync(source);
             expect.fail(err);
             done();
         })
     });
-    
+
     it("should use a mocked UI5 lib from a FS path and install in custom location in $project_dir", function (done) {
         "use strict";
         let fromMock = path.join(process.cwd(), "test", "example.zip");
         let to = "local1/ui5/dir";
         let source = path.join(process.cwd(), to);
-        Promise.resolve().then( () => {
+        Promise.resolve().then(() => {
             return install(fromMock, to);
-        }).then( () => {
+        }).then(() => {
             let iFiles = glob.sync(`${source}/**/*dbg.js`).length;
             expect(iFiles).to.be.greaterThan(0);
             fs.removeSync(source);
             done();
-        }).catch( err => {
+        }).catch(err => {
             fs.removeSync(source);
             expect.fail(err);
             done();
         })
+    });
+
+    it("should recognize a non-existing .zip of a URL", function (done) {
+        const {install} = require('../lib/commands'); // reset proxyquired/stubbed functionality
+        let from = "http://metalodge.com/doesntexist.zip";
+        let to = "local/ui5/dir";
+        install(from, to)
+            .catch(err => {
+                expect(err).to.include("doesn't seem to exist");
+                done();
+            })
     })
 });
